1a8dcfccee1f12ccc6bb7ca87426ca51bf659d5e
= <=>
2024-07-26 20:15:01 +0530

    push

---
+++ server.js
@@ -0,0 +1,19 @@
+// importing the aaplication
+const http = require("http");     
+
+// server should be dedicated to a specific port number
+const PORT = 8081;
+ 
+
+//writehead is used to pass some status code, write is used to show response, respose end is used to release the response.
+
+http.createServer((req, res)=> {
+   res.writeHead(200,{"Content-Type": "text/html"});
+   res.write("<h1>Hello World Everyone 123456 :-) </h1>");
+   res.end();
+})
+.listen(PORT,()=>{
+    console.log(`NodeJs Server is Up and Running Successfully on Port ${PORT}`)
+})
+
+// http://localhost:8081
